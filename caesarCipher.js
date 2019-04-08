2350d0a7e294d56f66c45aae78c4326fb33f6326
dddlovric <dddlovric@gmail.com>
2019-04-08 14:56:12 +0200

    gotovo

--- caesarCipher.js
+++ caesarCipher.js
@@ -1,5 +1,28 @@
-function  caesarCipher(string, number) {
+function caesarCipher(string, number) {
+    number = number % 26;
+    let lowerCaseString = string.toLowerCase();
+    let alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+    let newString ='';
 
+    for (var i = 0; i < lowerCaseString.length; i++) {
+        let currentLetter = lowerCaseString[i];
+        if (currentLetter === ' ') {
+            newString += currentLetter;
+            continue;
+        }
+        let currentIndex = alphabet.indexOf(currentLetter);
+        let newIndex = currentIndex + number;
+        if (newIndex > 25) newIndex = newIndex - 26;
+        if (newIndex < 0) newIndex = 26 + newIndex;
+        if (string[i] === string[i].toUpperCase()) {
+            newString += alphabet[newIndex].toUpperCase()
+        }
+        else newString += alphabet[newIndex];
+    }
+
+    return newString;
 }
 
-caesarCipher('',);
+const rezultat = caesarCipher('Zoo Keeper',2); //Bqq Mggrgt
+
+console.log(rezultat);
